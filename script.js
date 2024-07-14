947a6245cd0c0d9b20f085ee478df19b0e5a89a0
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-07-14 18:03:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// const scroll = new LocomotiveScroll({
+//           el: document.querySelector('.main'),
+//           smooth: true
+//       });
+
+// ScrollReveal().reveal('#hero-section');
+// ScrollReveal().reveal('#breeder-section');
