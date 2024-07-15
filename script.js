d48d6813633d5f795a7ed801b6fdbee33f9c0916
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-07-15 16:53:01 +0530

    Done

--- script.js
+++ script.js
@@ -5,3 +5,15 @@
 
 // ScrollReveal().reveal('#hero-section');
 // ScrollReveal().reveal('#breeder-section');
+
+let menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+          if(menuList.style.maxHeight == "0px"){
+                    menuList.style.maxHeight = "300px"
+          }
+          else{
+                    menuList.style.maxHeight = "0px";
+          }
+}
